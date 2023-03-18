69619e44582088b7264e2898398eee7b0405d1bc
Juan Gamonoso <juan.gamonoso@hotmail.com>
2023-03-18 01:17:30 +0100

    add validarLigaManager

--- gestores/gestormanager.php
+++ gestores/gestormanager.php
@@ -9,4 +9,19 @@
 
 		return ($resultado->num_rows > 0);
 	}
+
+	function validarLigaManager($user, $pass)
+	{
+		$sql = "select fk_equipo_liga from manager ma, equipo eq where ma.manager_login='".$user."' and ma.manager_password='".md5($pass)."' and ma.pk_manager=eq.fk_equipo_manager";
+
+		$result = consultarSql($sql);
+
+		if ($result->num_rows > 0) {
+			$row = $result->fetch_assoc();
+
+			$liga = $row["fk_equipo_liga"];
+
+			return $liga;
+		}
+	}
 ?>
