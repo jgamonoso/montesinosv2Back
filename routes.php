61dc0b94e121cc175f2f948516f45d347f6ff3b0
Juan Gamonoso <juan.gamonoso@hotmail.com>
2023-04-26 23:32:17 +0200

    change liga/noticias routes

--- routes.php
+++ routes.php
@@ -6,9 +6,9 @@ function getRoutes()
     // Define las rutas y sus archivos correspondientes
     $routes = [
         '/api/v2/login' => 'v2/login.php',
-        '/api/v2/noticias' => 'v2/noticias.php',
         '/api/v2/miequipo' => 'v2/miequipo.php',
         '/api/v2/liga' => 'v2/liga.php',
+            '/api/v2/liga/noticias' => 'v2/noticias.php',
     ];
 
     return $routes;
